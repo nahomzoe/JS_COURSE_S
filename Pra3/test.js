c86f2885952dd26a35da1e47f78949a8d6c4d193
Asfaw Nahom <s2200108@edu.bc.fi>
2022-03-01 11:05:41 +0200

    mini project

--- Pra3/test.js
+++ Pra3/test.js
@@ -66,3 +66,43 @@ console.log(firstValue);*/
 //}
 
 const upperCasedName = person.name?.toUpperCase();*/
+/*let p = new Promise((resolve, reject) => {
+  reject(Error("This fails"));
+});
+p.catch((error) => console.log(error.message));
+p.catch((error) => console.log(error.message));*/
+
+/*let p = new Promise((resolve, reject) => {
+  return promise.reject(Error("This fails"));
+});
+p.catch((error) => console.log(error.message));
+p.catch((error) => console.log(error.message));*/
+/*let p = new Promise((resolve, reject) => {
+  reject(Error("This fails"));
+})
+  .catch((error) => console.log(error))
+  .then((error) => console.log(error));*/
+
+/*let p = new Promise((resolve, reject) => {
+  reject(Error("This fails"));
+})
+  .catch((error) => console.log(error.message))
+  .catch((error) => console.log(error.message));*/
+
+/*let p = new Promise((resolve, reject) => {
+  resolve("success");
+})
+  .then(() => {
+    throw Error("Ih no");
+  })
+  .catch((error) => {
+    return "actually,that works";
+  })
+ .catch((error) => console.log(error.message));*/
+Promise.resolve("Success")
+  .then((data) => {
+    return data.toUpperCase();
+  })
+  .then((data) => {
+    console.log(data);
+  });
