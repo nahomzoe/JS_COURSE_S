fa834da590ab30b0c7396964b4d3d72167b0ec23
Asfaw Nahom <s2200108@edu.bc.fi>
2022-03-14 15:25:41 +0200

    last of monday

--- Pra7/misc/misc2/misc2.js
+++ Pra7/misc/misc2/misc2.js
@@ -1,23 +1,36 @@
-'use strict';
+"use strict";
 
 /**
 Every time you click on the Start game button, it is currently calling the startGame() and logAnalytics() functions.
 Update the event listener such that it only calls the startGame() once, but keeps calling logAnalytics() every time.
 **/
 
-const button = document.querySelector("#app-button");
+/*const button = document.querySelector("#app-button");
+let gameStarted = false;
 button.addEventListener("click", () => {
+  if (!gameStarted) {
     startGame();
+    gameStarted = true;
+  } else {
     logAnalytics();
+  }
+});*/
+const button = document.querySelector("#app-button");
+let gameStarted = false;
+button.addEventListener("click", () => {
+  while (!gameStarted) {
+    startGame();
+    gameStarted = true;
+  }
+  logAnalytics();
 });
-
 
 // do not modify
 function startGame() {
-    console.log("game started!");
+  console.log("game started!");
 }
 
 // do not modify
 function logAnalytics() {
-    console.log("log analytics");
+  console.log("log analytics");
 }
